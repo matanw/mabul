58abc683ac92422ac5756f4a7b7421c5d6f19a93
Matan  Wiesner <matan.wiesner@oracle.com>
2018-10-30 17:55:50 +0200

    enum style

--- maman14/myassembly/first_stage.h
+++ maman14/myassembly/first_stage.h
@@ -4,17 +4,15 @@
 #include "list.h"
 #include <stdio.h>
 
-enum OPERATION {
+typedef enum OPERATION {
     Mov = 0, Cmp = 1, Add = 2, Sub = 3, Not = 4, Clr = 5, Lea = 6, Inc = 7, Dec = 8,
     Jmp = 9, Bne = 10, Red = 11, Prn = 12, Jsr = 13, Rst = 14, Stop = 15, Unknown = 16
-};
-typedef enum OPERATION operation;
+} operation;
 
 
-enum ADDRESSING_METHOD {
+typedef enum ADDRESSING_METHOD {
     ImmediateAddressing = 1, DirectAddressing = 3, RegisterAddressing = 5
-};
-typedef enum ADDRESSING_METHOD addressing_method;
+} addressing_method;
 
 typedef struct command_line {
     int bits;
